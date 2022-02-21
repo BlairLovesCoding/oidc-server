732107d602edaf1de94908e931e9773861c4b49d
Blair Wu <blair.wu@airbnb.com>
2022-02-21 12:51:19 -0500

    initial setup

---
+++ src/test/java/com/netflix/oidcserver/OidcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.oidcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
